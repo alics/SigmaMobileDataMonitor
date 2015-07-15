6c44d550034454612c3855a0490ffda4f39e6b58
epsilonzer0 <abbasi.eng@gmail.com>
2015-07-15 16:31:12 +0430

    add

--- app/src/main/java/com/zohaltech/app/mobiledatamonitor/dal/PackageHistories.java
+++ app/src/main/java/com/zohaltech/app/mobiledatamonitor/dal/PackageHistories.java
@@ -4,4 +4,27 @@ package com.zohaltech.app.mobiledatamonitor.dal;
  * Created by Ali on 7/15/2015.
  */
 public class PackageHistories {
+    static final String TableName = "PackageHistories";
+    static final String Id = "Id";
+    static final String DataPackageId = "DataPackageId";
+    static final String StartDateTime = "StartDateTime";
+    static final String EndDateTime = "EndDateTime";
+    static final String SimId = "SimId";
+    static final String Active = "Active";
+
+
+    static final String CreateTable = "CREATE TABLE " + TableName + " (" +
+            Id + " INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, " +
+            DataPackageId + " INTEGER REFERENCES " + DataPackages.TableName + " (" + DataPackages.Id + "), " +
+            StartDateTime + "DATE NOT NULL," +
+            EndDateTime + " DATE NOT NULL," +
+            SimId + " INTEGER  NOT NULL ," +
+            Active + " BOOLEAN   );";
+//            SecondaryTraffic + " BIGINT   ," +
+//            SecondaryTrafficStartTime + " TIME   ," +
+//            SecondaryTrafficEndTime + " TIME   ," +
+//            UssdCode + " VARCHAR(50)   ," +
+//            Custom + " BOOLEAN   )";
+
+    static final String DropTable = "Drop Table If Exists " + TableName;
 }
