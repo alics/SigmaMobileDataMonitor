ffdacb8bab34b7dbc3f144bdeeb9c4fd1823a9b1
epsilonzer0 <abbasi.eng@gmail.com>
2015-07-15 16:31:29 +0430

    add

--- app/src/main/java/com/zohaltech/app/mobiledatamonitor/dal/UsageLogs.java
+++ app/src/main/java/com/zohaltech/app/mobiledatamonitor/dal/UsageLogs.java
@@ -4,4 +4,21 @@ package com.zohaltech.app.mobiledatamonitor.dal;
  * Created by Ali on 7/15/2015.
  */
 public class UsageLogs {
+    static final String TableName = "UsageLogs";
+    static final String Id = "Id";
+    static final String TrafficBytes = "TrafficBytes";
+    static final String LogDateTime = "LogDateTime";
+//    static final String EndDateTime = "EndDateTime";
+//    static final String SimId = "SimId";
+//    static final String Active = "Active";
+
+
+    static final String CreateTable = "CREATE TABLE " + TableName + " (" +
+            Id + " INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, " +
+            TrafficBytes + "BIGINT NOT NULL, " +
+            LogDateTime + "DATE );";
+//            EndDateTime + " DATE" +
+//            SimId + " INTEGER  NOT NULL ," +
+//            Active + " BOOLEAN   )" ;
+//}
 }
