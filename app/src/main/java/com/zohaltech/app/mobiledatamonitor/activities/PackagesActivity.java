f8b30ce8980a94e6580e6552578cac6381d89c1f
ostademast <ostademast@yahoo.com>
2015-07-20 20:42:56 +0430

    sadjad

--- app/src/main/java/com/zohaltech/app/mobiledatamonitor/activities/PackagesActivity.java
+++ app/src/main/java/com/zohaltech/app/mobiledatamonitor/activities/PackagesActivity.java
@@ -1,13 +1,19 @@
 package com.zohaltech.app.mobiledatamonitor.activities;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ExpandableListView;
+import android.widget.TextView;
+
 import com.zohaltech.app.mobiledatamonitor.R;
 import com.zohaltech.app.mobiledatamonitor.adapters.ExpandablePackageAdapter;
 import com.zohaltech.app.mobiledatamonitor.adapters.PackagePagerAdapter;
+import com.zohaltech.app.mobiledatamonitor.classes.App;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -16,7 +22,8 @@ import java.util.List;
 
 public class PackagesActivity extends EnhancedActivity{
 
-
+    ViewPager pagerPackages;
+    TabLayout tabOperators;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -24,16 +31,31 @@ public class PackagesActivity extends EnhancedActivity{
         setContentView(R.layout.activity_packages);
 
         // Get the ViewPager and set it's PagerAdapter so that it can display items
-        ViewPager pagerPackages = (ViewPager) findViewById(R.id.pagerPackages);
+        pagerPackages = (ViewPager) findViewById(R.id.pagerPackages);
         pagerPackages.setAdapter(new PackagePagerAdapter(getSupportFragmentManager(), PackagesActivity.this));
 
         // Give the TabLayout the ViewPager
-        TabLayout tabOperators = (TabLayout) findViewById(R.id.tabOperators);
+        tabOperators = (TabLayout) findViewById(R.id.tabOperators);
+
+        //((ViewGroup)tabOperators.getChildAt(0)).getChildAt().setTypeface(App.persianFont);
         tabOperators.setupWithViewPager(pagerPackages);
 
-
+        changeTabsFont();
     }
 
+    private void changeTabsFont() {
 
-
+        ViewGroup vg = (ViewGroup) tabOperators.getChildAt(0);
+        int tabsCount = vg.getChildCount();
+        for (int j = 0; j < tabsCount; j++) {
+            ViewGroup vgTab = (ViewGroup) vg.getChildAt(j);
+            int tabChildsCount = vgTab.getChildCount();
+            for (int i = 0; i < tabChildsCount; i++) {
+                View tabViewChild = vgTab.getChildAt(i);
+                if (tabViewChild instanceof TextView) {
+                    ((TextView) tabViewChild).setTypeface(App.persianFont);
+                }
+            }
+        }
+    }
 }
