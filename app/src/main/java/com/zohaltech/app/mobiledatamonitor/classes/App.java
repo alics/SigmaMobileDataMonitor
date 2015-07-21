b84768de8b10df71045ef17092d930ed97e87918
ostademast <ostademast@yahoo.com>
2015-07-21 13:16:06 +0430

    sadjad

--- app/src/main/java/com/zohaltech/app/mobiledatamonitor/classes/App.java
+++ app/src/main/java/com/zohaltech/app/mobiledatamonitor/classes/App.java
@@ -7,15 +7,14 @@ import android.content.SharedPreferences;
 import android.graphics.Typeface;
 import android.os.Handler;
 import android.preference.PreferenceManager;
-import android.support.v4.app.FragmentActivity;
 
 public class App extends Application {
 
-    public static Context           context;
+    public static Context context;
     public static Activity currentActivity;
     public static SharedPreferences preferences;
-    public static Typeface          englishFont;
-    public static Typeface          persianFont;
+    public static Typeface englishFont;
+    public static Typeface persianFont;
     public static Handler handler;
 
     @Override
