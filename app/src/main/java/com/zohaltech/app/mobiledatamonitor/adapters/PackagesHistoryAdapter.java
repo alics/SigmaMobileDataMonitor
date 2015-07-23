c41e90a284f743628c5a7c9a661cc8ce946345f2
alics <ali.faghani@gmail.com>
2015-07-23 12:59:11 +0430

    Packages History activity and adapter

---
+++ app/src/main/java/com/zohaltech/app/mobiledatamonitor/adapters/PackagesHistoryAdapter.java
@@ -0,0 +1,62 @@
+package com.zohaltech.app.mobiledatamonitor.adapters;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.zohaltech.app.mobiledatamonitor.R;
+import com.zohaltech.app.mobiledatamonitor.classes.App;
+import com.zohaltech.app.mobiledatamonitor.classes.SolarCalendar;
+import com.zohaltech.app.mobiledatamonitor.dal.DataPackages;
+import com.zohaltech.app.mobiledatamonitor.entities.PackageHistory;
+
+import java.util.ArrayList;
+
+public class PackagesHistoryAdapter extends ArrayAdapter<PackageHistory> {
+    public PackagesHistoryAdapter(ArrayList<PackageHistory> packageHistories) {
+        super(App.context, R.layout.adapter_packages_history, packageHistories);
+    }
+
+    private static class ViewHolder {
+
+        TextView     txtPackageDesc;
+        TextView     txtActivateDate;
+        TextView     txtExpDate;
+        TextView     txtStatus;
+        LinearLayout layoutPackageHistory;
+
+        public ViewHolder(View view) {
+            txtPackageDesc = (TextView) view.findViewById(R.id.txtPackageDesc);
+            txtActivateDate = (TextView) view.findViewById(R.id.txtActivateDate);
+            txtExpDate = (TextView) view.findViewById(R.id.txtExpDate);
+            txtStatus = (TextView) view.findViewById(R.id.txtStatus);
+            layoutPackageHistory = (LinearLayout) view.findViewById(R.id.layoutPackageHistory);
+        }
+
+        public void fill(final ArrayAdapter<PackageHistory> adapter, final PackageHistory item, final int position) {
+            txtPackageDesc.setText(DataPackages.selectPackagesById(item.getDataPackageId()).get(0).getTitle());
+            txtActivateDate.setText(SolarCalendar.getShamsiDateTime(item.getStartDateTime()));
+            txtExpDate.setText(SolarCalendar.getShamsiDateTime(item.getEndDateTime()));
+            txtStatus.setText(item.getActive() ? "فعال" : "غیر فعال");
+        }
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+
+        PackageHistory item = getItem(position);
+        if (convertView == null) {
+            convertView = App.inflater.inflate(R.layout.adapter_packages_history, parent, false);
+            holder = new ViewHolder(convertView);
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        holder.fill(this, item, position);
+        return convertView;
+    }
+}
