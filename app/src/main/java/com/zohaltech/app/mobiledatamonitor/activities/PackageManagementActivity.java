9f0f7644b5d376b73bc16ec2de24eaca2c40ef1b
epsilonzer0 <abbasi.eng@gmail.com>
2015-07-30 12:02:45 +0430

    Package management layout designed

---
+++ app/src/main/java/com/zohaltech/app/mobiledatamonitor/activities/PackageManagementActivity.java
@@ -0,0 +1,16 @@
+package com.zohaltech.app.mobiledatamonitor.activities;
+
+
+import android.os.Bundle;
+
+import com.zohaltech.app.mobiledatamonitor.R;
+
+public class PackageManagementActivity extends EnhancedActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_package_management);
+
+    }
+}
