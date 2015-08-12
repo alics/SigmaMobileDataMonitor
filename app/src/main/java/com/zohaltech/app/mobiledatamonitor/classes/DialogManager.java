ae0ace2fecd3ff25f4857cac4eee081e3f2ce11c
ostademast <ostademast@yahoo.com>
2015-08-12 15:00:09 +0430

    add showChoiceDialog method

--- app/src/main/java/com/zohaltech/app/mobiledatamonitor/classes/DialogManager.java
+++ app/src/main/java/com/zohaltech/app/mobiledatamonitor/classes/DialogManager.java
@@ -53,4 +53,48 @@ public final class DialogManager {
         });
         dialog.show();
     }
+
+    public static void showChoiceDialog(
+            Activity activity
+            , final String caption
+            , final String message
+            , String positiveButtonText
+            , String negativeButtonText
+            , final Runnable onDialogShown
+            , final Runnable onPositiveButtonClick
+            , final Runnable onNegativeButtonClick) {
+        if (onDialogShown != null) {
+            onDialogShown.run();
+        }
+        final Dialog dialog = new Dialog(activity);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.dialog_confirmation);
+        dialog.setCanceledOnTouchOutside(true);
+        dialog.setCancelable(true);
+        TextView txtDialogCaption = (TextView) dialog.findViewById(R.id.txtDialogCaption);
+        TextView txtDialogMessage = (TextView) dialog.findViewById(R.id.txtDialogMessage);
+        Button positiveButton = (Button) dialog.findViewById(R.id.positiveButton);
+        Button negativeButton = (Button) dialog.findViewById(R.id.negativeButton);
+        txtDialogCaption.setText(caption);
+        txtDialogMessage.setText(message);
+        positiveButton.setText(positiveButtonText);
+        negativeButton.setText(negativeButtonText);
+
+        positiveButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onPositiveButtonClick.run();
+                dialog.dismiss();
+            }
+        });
+
+        negativeButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onNegativeButtonClick.run();
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
+    }
 }
