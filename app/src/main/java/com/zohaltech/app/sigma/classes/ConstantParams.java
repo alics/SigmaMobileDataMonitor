7e6d032fef6d2e26e8333cb062fa028d3ac8d7ed
ostademast <ostademast@yahoo.com>
2015-09-06 14:48:43 +0430

    introduction

--- app/src/main/java/com/zohaltech/app/sigma/classes/ConstantParams.java
+++ app/src/main/java/com/zohaltech/app/sigma/classes/ConstantParams.java
@@ -6,14 +6,14 @@ import android.os.Environment;
 import com.zohaltech.app.sigma.R;
 
 public final class ConstantParams {
-    private static String licenseFilePath = Environment.getExternalStorageDirectory() + App.context.getString(R.string.vps);
-    private static String apiSecurityKey = App.context.getString(R.string.jan);
-    private static String fileName = App.context.getString(R.string.sys);
-    private static String token = App.context.getString(R.string.cs);
-    private static String secretKey = App.context.getString(R.string.sdj);
-    private static String iv = App.context.getString(R.string.v);
-    private static String seven = App.context.getString(R.string.seven);
-    private static String base64EncodedPublicKey = App.context.getString(R.string.base64EncodedPublicKey);
+    private static String licenseFilePath        = Environment.getExternalStorageDirectory() + App.context.getString(R.string.topol);
+    private static String apiSecurityKey         = App.context.getString(R.string.jan);
+    private static String fileName               = App.context.getString(R.string.doosh);
+    //private static String token                  = App.context.getString(R.string.cs);
+    private static String secretKey              = App.context.getString(R.string.sdj);
+    private static String iv                     = App.context.getString(R.string.mongol);
+    private static String seven                  = App.context.getString(R.string.goon);
+    private static String base64EncodedPublicKey = App.context.getString(R.string.bare_sala);
 
     public static String getLicenseFilePath() {
         return licenseFilePath;
@@ -23,9 +23,9 @@ public final class ConstantParams {
         return fileName;
     }
 
-    public static String getToken() {
-        return token;
-    }
+    //public static String getToken() {
+    //    return token;
+    //}
 
     public static String getSecretKey() {
         return secretKey;
