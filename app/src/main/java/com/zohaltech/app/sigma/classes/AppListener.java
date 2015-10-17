0f6bd9e78b1067e83e97b93d41c339fb07540715
ali <ali.faghani@gmail.com>
2015-10-17 16:28:22 +0330

    AppListener

--- app/src/main/java/com/zohaltech/app/sigma/classes/AppListener.java
+++ app/src/main/java/com/zohaltech/app/sigma/classes/AppListener.java
@@ -3,6 +3,8 @@ package com.zohaltech.app.sigma.classes;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.widget.Toast;
 
@@ -18,16 +20,30 @@ public class AppListener extends BroadcastReceiver {
             Bundle b = intent.getExtras();
             int uid = b.getInt(Intent.EXTRA_UID);
             String[] packages = context.getPackageManager().getPackagesForUid(uid);
+            String appName=getAppName(packages[0]);
 
-            MyToast.show("ACTION_PACKAGE_ADDED"+packages[1], Toast.LENGTH_LONG);
+            MyToast.show("ACTION_PACKAGE_ADDED  " + appName, Toast.LENGTH_LONG);
         } else if (intent.getAction().equals(Intent.ACTION_PACKAGE_REMOVED)) {
             Bundle b = intent.getExtras();
             int uid = b.getInt(Intent.EXTRA_UID);
 
-            String[] packages = context.getPackageManager().getPackagesForUid(uid);
+          //  String[] packages = context.getPackageManager().getPackagesForUid(uid);
+           // String appName=getAppName(packages[0]);
 
-            MyToast.show("ACTION_PACKAGE_ADDED"+packages[0], Toast.LENGTH_LONG);
-            MyToast.show("ACTION_PACKAGE_REMOVED", Toast.LENGTH_LONG);
+           // MyToast.show("ACTION_PACKAGE_ADDED" + packages[0], Toast.LENGTH_LONG);
+            MyToast.show("ACTION_PACKAGE_REMOVED "+uid, Toast.LENGTH_LONG);
         }
+    }
+
+    private String getAppName(String packageName) {
+        final PackageManager pm = App.context.getApplicationContext().getPackageManager();
+        ApplicationInfo ai;
+        try {
+            ai = pm.getApplicationInfo(App.context.getPackageName(), 0);
+        } catch (final PackageManager.NameNotFoundException e) {
+            ai = null;
+        }
+        return (String) (ai != null ? pm.getApplicationLabel(ai) : "(unknown)");
+
     }
 }
