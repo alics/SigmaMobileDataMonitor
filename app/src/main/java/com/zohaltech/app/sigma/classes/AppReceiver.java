84db28d70589a0eada7e0852f4ad0b870338f2b7
Ali <ali.faghani@gmail.com>
2015-12-13 13:42:42 +0330

    application data usages

--- app/src/main/java/com/zohaltech/app/sigma/classes/AppReceiver.java
+++ app/src/main/java/com/zohaltech/app/sigma/classes/AppReceiver.java
@@ -18,35 +18,35 @@ public class AppReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        ApplicationInfo info;
-        Uri uri = intent.getData();
-        try {
-            String pkgName = uri.getSchemeSpecificPart();
-            if (intent.getAction().equals(Intent.ACTION_PACKAGE_ADDED)) {
-                info = context.getPackageManager().getApplicationInfo(pkgName, PackageManager.GET_META_DATA);
-
-                Application app = Applications.getAppById(info.uid);
-                if (app == null) {
-                    String appName = context.getPackageManager().getApplicationLabel(info).toString();
-                    Application addedApp = new Application(info.uid, appName, pkgName, false);
-                    Applications.insert(addedApp);
-                } else {
-                    app.setRemoved(false);
-                    Applications.update(app);
-                }
-
-                MyToast.show("package added : " + appName, Toast.LENGTH_LONG);
-                //MyToast.show("app name : " + appName, Toast.LENGTH_LONG);
-                //MyToast.show("uid : " + info.uid, Toast.LENGTH_LONG);
-                //MyToast.show("icon : " + info.icon, Toast.LENGTH_LONG);
-            } else if (intent.getAction().equals(Intent.ACTION_PACKAGE_REMOVED)) {
-                Application app = Applications.getAppByPackage(pkgName);
-                app.setRemoved(true);
-                Applications.update(app);
-                //MyToast.show("package removed : " + pkgName, Toast.LENGTH_LONG);
-            }
-        } catch (PackageManager.NameNotFoundException e) {
-            e.printStackTrace();
-        }
+        //    ApplicationInfo info;
+        //    Uri uri = intent.getData();
+        //    try {
+        //        String pkgName = uri.getSchemeSpecificPart();
+        //        if (intent.getAction().equals(Intent.ACTION_PACKAGE_ADDED)) {
+        //            info = context.getPackageManager().getApplicationInfo(pkgName, PackageManager.GET_META_DATA);
+        //
+        //            Application app = Applications.getAppById(info.uid);
+        //            if (app == null) {
+        //                String appName = context.getPackageManager().getApplicationLabel(info).toString();
+        //                Application addedApp = new Application(info.uid, appName, pkgName, false);
+        //                Applications.insert(addedApp);
+        //            } else {
+        //                app.setRemoved(false);
+        //                Applications.update(app);
+        //            }
+        //
+        //            MyToast.show("package added : " + appName, Toast.LENGTH_LONG);
+        //            //MyToast.show("app name : " + appName, Toast.LENGTH_LONG);
+        //            //MyToast.show("uid : " + info.uid, Toast.LENGTH_LONG);
+        //            //MyToast.show("icon : " + info.icon, Toast.LENGTH_LONG);
+        //        } else if (intent.getAction().equals(Intent.ACTION_PACKAGE_REMOVED)) {
+        //            Application app = Applications.getAppByPackage(pkgName);
+        //            app.setRemoved(true);
+        //            Applications.update(app);
+        //            //MyToast.show("package removed : " + pkgName, Toast.LENGTH_LONG);
+        //        }
+        //    } catch (PackageManager.NameNotFoundException e) {
+        //        e.printStackTrace();
+        //    }
     }
 }
